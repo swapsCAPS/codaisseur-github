2807d4bbf774e3965b273ad2f4cfc6e2d7371699
stofstik <verstegen.daan@gmail.com>
2016-11-17 18:10:59 +0100

    fix wrapper

--- test/wrapper.js
+++ test/wrapper.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { mount } from 'enzyme'
-import store from '../src/store'
+import store from '../client/store'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import CodaisseurGithub from '../client/styles/base-theme'
 
 export default (node, nodeContext = {}) => {
   let context = Object.assign({}, nodeContext)
 
   return mount(
     <Provider store={store}>
-      {node}
+      <MuiThemeProvider>
+        {node}
+    </MuiThemeProvider>
     </Provider>, { context }
   )
 }
